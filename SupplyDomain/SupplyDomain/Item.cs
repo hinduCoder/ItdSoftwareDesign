93c3a405a000279ffd7674da3878942b7178e3a4
Andrew <zub0194@gmail.com>
2015-02-20 18:57:13 +0300

    Added Item and OrderedItem

---
+++ SupplyDomain/SupplyDomain/Item.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyDomain
+{
+    class Item
+    {
+        private string name;
+        private string description;
+        private string article;
+
+        public string Name
+        {
+            get 
+            {
+                return name;
+            }
+        }
+
+        public string Descripton
+        {
+            get
+            {
+                return description;
+            }
+        }
+
+        public string Article
+        {
+            get
+            {
+                return article;
+            }
+        }
+
+        public Item(string _name, string _description, string _article)
+        {
+            name = _name;
+            description = _description;
+            article = _article;
+        }
+    }
+}
