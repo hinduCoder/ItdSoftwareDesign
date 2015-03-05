cf7184d283ce4d0c66e61b3a125ea9f497b2c524
Anton <antngribv@yandex.ru>
2015-03-05 19:20:15 +0300

    begin of menu

---
+++ SupplyDomain/SupplyClient/ActionExecutionContextExtension.cs
@@ -0,0 +1,13 @@
+﻿using Feonufry.CUI.Actions;
+
+namespace SupplyClient
+{
+    public static class ActionExecutionContextExtension
+    {
+        public static string InputString(this ActionExecutionContext context, string prompt)
+        {
+            context.Out.Write("{0}: ");
+            return context.In.ReadLine();
+        }
+    }
+}
---
+++ SupplyDomain/SupplyClient/ContractActions.cs
@@ -0,0 +1,7 @@
+﻿namespace SupplyClient
+{
+    public class ContractActions
+    {
+         
+    }
+}
