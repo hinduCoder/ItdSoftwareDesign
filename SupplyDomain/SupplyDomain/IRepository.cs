07dc04ba1ad84951e28d7c2755eb174e83d0dbfa
Igor Grishin <bigspawn@yandex.ru>
2015-03-05 17:41:57 +0300

    Create IRepository.cs and ItemRepository.cs

---
+++ SupplyDomain/SupplyDomain/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyDomain
+{
+    interface IRepository
+    {
+        void Add(Item item);
+        void Delete(Item item);
+        Item FindByArticle(String article);
+    }
+}
--- SupplyDomain/SupplyDomain/Item.cs
+++ SupplyDomain/SupplyDomain/Item.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SupplyDomain
 {
-    class Item
+    public class Item
     {
         private string _name;
         private string _description;
